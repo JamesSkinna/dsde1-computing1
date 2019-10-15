466e1c284820a16b72fb94c047446fa3b3077bb9
James Skinner <james.skinner19@imperial.ac.uk>
2019-10-15 12:01:53 +0100

    Adding a file

---
+++ Week-2/ExtensionGuessingGame.py
@@ -0,0 +1,6 @@
+import random as R
+print("Enter a number between 1 and 10 (inclusive): ")
+GuessedNum = int(input())
+RandomNumber = R.randint(1, 11)
+print("The random number was: " + str(RandomNumber))
+print(str(RandomNumber == GuessedNum))
