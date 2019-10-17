af5d6f028b1521a88a15c0d31e6923e532775d14
James Skinner <james.skinner19@imperial.ac.uk>
2019-10-17 14:14:55 +0100

    Fixed all functions

--- Week-3/toys.py
+++ Week-3/toys.py
@@ -9,20 +9,23 @@ with functions.
 # write a function that adds 1
 # to the input and prints the result
 def inc(a):
+    a = a + 1
     print(a)
 
 
 # write a function that adds 1
 # to the input and returns the result
 def inc_return(a):
-    return # hint this is incomplete
+    a = a + 1
+    return a # hint this is incomplete
 
 
 # write a function that adds
 # the two input numbers together
 # and returns the sum
 def sum(a, b):
-    return
+    sum = a + b
+    return sum
 
 
 # write a function that takes two
@@ -30,14 +33,19 @@ def sum(a, b):
 # sum() and then increments the sum
 # using inc_return
 def sum_inc(a, b):
-    return
+    summed = sum(a, b)
+    incSum = inc_return(summed)
+    return incSum
 
 
 # write a function that returns a 
 # boolean (True or False) for whether 
 # the input number is even
 def is_even(a):
-    return
+    if (a % 2) == 0:
+        return True
+    else:
+        return False
 
 
 # create for loop that takes a string
@@ -49,5 +57,17 @@ def is_even(a):
 def string_repeat(phrase, repeat):
     # hint: you can add strings together 
     # in order to concatenate them
-    return
+    concat = phrase
+    for x in range(repeat - 1):
+        concat = concat + phrase
+    return concat
 
+# def main():
+ #   test = string_repeat("Test", 3)
+  #  print(test)
+   # print(is_even(4))
+    #print(is_even(5))
+#
+
+# if __name__ == "__main__":
+#    main() 
