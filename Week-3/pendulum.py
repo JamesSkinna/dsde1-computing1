409ebfd832b7d5ef51dd16f8daf925d9ace789fb
James Skinner <james.skinner19@imperial.ac.uk>
2019-10-22 11:18:30 +0100

    Improved linter

--- Week-3/pendulum.py
+++ Week-3/pendulum.py
@@ -1,16 +1,17 @@
 import math
 
 def period(L, g):
-        if isinstance(L, str) or isinstance(g, str):
-            raise TypeError
-        if (g <= 0) or (L<0):
-            raise ValueError
-        T = (2*math.pi)*(math.sqrt(L/g))
-        return T
+    if isinstance(L, str) or isinstance(g, str):
+        raise TypeError
+    if (g <= 0) or (L < 0):
+        raise ValueError
+    T = (2*math.pi)*(math.sqrt(L/g))
+    return T
 
-#def main():
-   # Test = period(6, 0)
-  #  print(Test)        
+def main():
+    Test = period(6, 0)
+    print(Test)
 
-#if __name__ == "__main__":
- #   main() 
+if __name__ == "__main__":
+    main()
+    
