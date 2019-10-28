0f162bb64a3813e662dea5b65f86cbb30a86dd94
James Skinner <james.skinner19@imperial.ac.uk>
2019-10-28 20:42:11 +0000

    Keywords Edited

--- Week-4/keywords.py
+++ Week-4/keywords.py
@@ -14,15 +14,15 @@ with default values.
 # it returns 'Hello and welcome to <place>'
 # if both user_name and place are provided
 # it returns 'Hello, <user_name>, and welcome to <place>
-def welcome_message(user_name, place):
+def welcome_message(user_name = None, place = None):
     if (user_name == None) and (place == None):
-        print("Hello and welcome")
-    else if (user_name !== None) and (place == None):
-        print("Hello, " & user_name & ", and welcome")
-    else if (user_name == None) and (place !== None):
-        print("Hello and welcome to " & place)
+        return "Hello and welcome"
+    elif (user_name != None) and (place == None):
+        return ("Hello, " + user_name + ", and welcome")
+    elif (user_name == None) and (place != None):
+        return ("Hello and welcome to " + place)
     else:
-        print("Hello, " & user_name & ", and welcome to " & place)
+        return ("Hello, " + user_name + ", and welcome to " + place)
         
 
 
@@ -37,20 +37,22 @@ def welcome_message(user_name, place):
 # (return list of all modes if there is a tie between multiple values)
 # if avg_type='mean', return the mean of the list
 # if avg_type='median', return the median of this list
-def list_average(list_of_num, avg_type):
-    if (avg_type == None) or avg_type = "mean":
+def list_average(list_of_num, avg_type = None):
+    if list_of_num == []:
+        return 0
+    elif (avg_type == None) or avg_type == "mean":
         count = 0
         sum = 0
         for nums in list_of_num:
             count += 1
             sum += nums
         mean = sum / count
-    else if avg_type == "mode":
+    elif avg_type == "mode":
         count_dictionary = {}
         for nums in list_of_num:
             current_count = 0
             if nums in count_dictionary:
                 current_count = count_dictionary[nums]
-            count_dictionary[nums] = current_count += 1
+            count_dictionary[nums] = current_count + 1
 
     
